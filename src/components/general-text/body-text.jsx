fcd6fd4d9ddff34135c5dc0f7e63a622ea44d4e1
darcpabla <dpsds.elt@uea.edu.br>
2024-08-03 04:18:21 -0400

    adding the link

--- src/components/general-text/body-text.jsx
+++ src/components/general-text/body-text.jsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 import '../form/login.css'
 
 /*manipular textos*/
 function BodyText(props){
     return(
         <div className={"signup-link"}>
-          <p>{props.text} <a href="#">{props.text2}</a>
+          <p>{props.text} <Link>{props.text2}</Link>
           </p> 
           
         </div>
